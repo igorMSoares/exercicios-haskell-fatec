9c09a65719e25e7850b955fefa42c2da0919ce69
igorMSoares <igorms.dev@gmail.com>
2025-09-29 19:40:11 -0300

    Exercícios 3.11 à 3.21

--- Cap03.hs
+++ Cap03.hs
@@ -225,3 +225,176 @@ func3_9 x y z = (reverse x, reverse y, reverse z)
 -- Exemplo: revNum 4 "FATEC" = "ETAFC"
 revNum :: String -> Int -> String
 revNum s n = reverse (take n s) ++ drop n s
+
+-- 3.11) Crie o tipo de dado Binario que pode ser Zero ou Um .
+-- Faça outro tipo de dado chamado Funcao que pode ser
+-- Soma2 , Maior , Menor ou Mult2 . Implemente a função
+-- aplicar que recebe uma Funcao e dois Binarios .
+-- Seu retorno consiste em executar a operação desejada.
+-- Exemplo: aplicar Soma2 Um Um = Zero
+data Binario = Zero | Um
+
+data Funcao = Soma2 | Maior | Menor | Mult2
+
+aplicar :: Funcao -> Binario -> Binario -> Binario
+aplicar Soma2 x Zero = x
+aplicar Soma2 Zero x = x
+aplicar Soma2 _ _ = Zero
+aplicar Maior x Zero = x
+aplicar Maior Zero x = x
+aplicar Maior _ _ = Um
+aplicar Menor _ Zero = Zero
+aplicar Menor Zero _ = Zero
+aplicar Menor _ _ = Um
+aplicar Mult2 _ Zero = Zero
+aplicar Mult2 Zero _ = Zero
+aplicar Mult2 _ _ = Um
+
+-- 3.12) Faça uma função chamada binList , usando list compreeshion,
+-- que recebe uma lista de Binarios (ver exercício anterior)
+-- e retorna outra lista com elemento somado Um e
+-- convertido para Int . Exemplo:
+-- binList [Um, Zero, Zero, Um, Zero] = [0,1,1,0,1]
+binToInt :: Binario -> Int
+binToInt Um = 1
+binToInt Zero = 0
+
+binList :: [Binario] -> [Int]
+binList bs = [binToInt (aplicar Soma2 b Um) | b <- bs]
+
+-- 3.13) Faça um novo tipo chamado Metros , que possui um
+-- \textit{value constructor}
+-- de mesmo nome, cujos parâmetros são:
+-- um Int que representa a dimensão, e
+-- um Double que representa o valor da medida
+-- e outro chamado MetragemInvalida .
+--
+-- Implemente as funções:
+-- areaQuadrado :: Metros -> Metros : calcula a área de um quadrado.
+-- areaRet :: Metros -> Metros -> Metros : calcula a área de um retângulo.
+-- areaCubo :: Metros -> Metros : calcula a área de um cubo.
+-- Exemplo:
+-- Prelude> areaQuadrado (Metros 1 2.0)
+-- Metros 2 4.0
+-- Use o pattern matching para ignorar as metragens erradas
+-- (calcular a área de um quadrado com um lado de dimensão 4 não é válido).
+data Metros = Metros Int Double | MetragemInvalida deriving (Show)
+
+areaQuadrado :: Metros -> Metros
+areaQuadrado (Metros 2 x) = Metros 2 (x * x)
+areaQuadrado _ = MetragemInvalida
+
+areaRet :: Metros -> Metros -> Metros
+areaRet (Metros 2 x) (Metros 2 y) = Metros 2 (x * y)
+areaRet _ _ = MetragemInvalida
+
+areaCubo :: Metros -> Metros
+areaCubo (Metros 3 x) = Metros 3 (6 * x * x)
+areaCubo _ = MetragemInvalida
+
+-- 3.14) Faça o novo tipo Valido que possui dois value constructors Sim e Nao .
+-- O value constructor Sim possui um parâmetro (campo) String .
+-- Implemente uma função isNomeValido que
+-- recebe um nome e
+-- retorna Nao caso aString seja vazia;
+-- caso contrário, Sim .
+data Valido = Sim' String | Nao' deriving (Show)
+
+isNomeValido :: String -> Valido
+isNomeValido "" = Nao'
+isNomeValido x = Sim' x
+
+-- 3.15) Refaça o exercício 3 do capítulo anterior usando record syntax
+-- e tipos com parâmetro (siga o exemplo da conversão de medidas SI para imperial).
+data SomeList a = SomeList {elements :: [a]} deriving (Show)
+
+espelhaStrList :: SomeList String -> SomeList String
+espelhaStrList SomeList {elements = strs} = SomeList {elements = reverse [reverse s | s <- strs]}
+
+-- 3.16) Faça o tipo Numero ,
+-- que possui um value constructor Ok com um campo double
+-- e outro value constructor Erro com um campo String .
+--
+-- Faça a função dividir que divida dois números e,
+-- caso o segundo número seja 0, emita um erro (use o pattern matching).
+-- Exemplo:
+-- Prelude> dividir (Ok 6) (Ok 5)
+-- Ok 1.2.
+data Numero = Ok Double | Erro String deriving (Show)
+
+dividir :: Numero -> Numero -> Numero
+dividir _ (Ok 0) = Erro "can not divide by zero"
+dividir (Ok x) (Ok y) = Ok (x / y)
+dividir (Erro x) _ = Erro x
+dividir _ (Erro x) = Erro x
+
+-- 3.17) Faça o tipo Cripto que possua dois values constructors
+-- Mensagem e Cifrado , ambos com um campo String e um value constructor Erro .
+-- Faça as funções encriptar e decriptar , seguindo cada exemplo a seguir.
+--
+-- Prelude> encriptar (Mensagem "FATEC")
+-- Cifrado "GBUFD"
+--
+-- Prelude> decriptar (Cifrado "DBTB")
+-- Mensagem "CASA"
+--
+-- Veja que a encriptação deve empurrar cada letra a frente e
+-- a decriptação faz o inverso, empurrando uma letra para trás.
+-- Use as funções succ e pred , e também list compreeshions.
+-- Não é possível encriptar mensagens cifradas e decriptar mensagens.
+data Cripto = Mensagem String | Cifrado String | Erro' deriving (Show)
+
+encriptar :: Cripto -> Cripto
+encriptar (Mensagem s) = Cifrado [succ c | c <- s]
+encriptar _ = Erro'
+
+decriptar :: Cripto -> Cripto
+decriptar (Cifrado s) = Mensagem [pred c | c <- s]
+decriptar _ = Erro'
+
+-- 3.18) Faça uma função encriptarTodos que encripta (ou dá erro)
+-- todos os elementos de um vetor de Cripto .
+encriptarTodos :: [Cripto] -> [Cripto]
+encriptarTodos = map encriptar
+
+-- 3.19) Tendo como base o exercício de conversão de medidas,
+-- crie uma função que faça conversão de câmbio.
+-- Você deve criar o tipo Cambio contendo
+-- os value constructors Euro , Real e Dollar .
+-- Crie também o tipo Moeda que possui os campos
+-- val:: Double e cur :: Cambio .
+-- Use record syntax e as taxas de conversão do dia no qual você fez o exercício.
+data Cambio = Euro | Real | Dollar deriving (Show, Eq)
+
+data Moeda = Moeda {val :: Double, cur :: Cambio} deriving (Show)
+
+converterMoeda :: Moeda -> Cambio -> Moeda
+converterMoeda (Moeda val c1) c2
+  | c1 == c2 = Moeda val c2
+  | otherwise = Moeda (val * taxa c1 c2) c2
+  where
+    taxa Real Euro = 0.16
+    taxa Dollar Euro = 0.85
+    taxa Euro Real = 6.25
+    taxa Dollar Real = 5.35
+    taxa Real Dollar = 0.19
+    taxa Euro Dollar = 1.17
+
+-- 3.20) Crie a função converterTodosReal que
+-- recebe uma lista de moedas e
+-- retorna outra lista de moedas
+-- com todos os seus elementos convertidos para Real .
+-- Use list compreenshion.
+converterTodosReal :: [Moeda] -> [Moeda]
+converterTodosReal ms = [converterMoeda m Real | m <- ms]
+
+-- 3.21) Crie a função maxMoeda que recebe uma lista de moedas
+-- e retorna o valor máximo absoluto (sem conversão alguma) dentre
+-- os campos val desta lista. Exemplo:
+--
+-- Prelude> maxMoeda [Moeda 3 Real, Moeda 7 Dollar, Moeda 1 Euro]
+-- 7
+--
+-- Use a função maximum .
+maxMoeda :: [Moeda] -> Double
+maxMoeda ms = maximum [val m | m <- ms]
